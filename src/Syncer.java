64ab38e3510758ca9588bbf099e058435b2bd7f7
imet057 <imet057@aucklanduni.ac.nz>
2022-05-03 15:44:10 +1200

    dot sync file functionality almost completed

--- src/Syncer.java
+++ src/Syncer.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,26 +26,54 @@ public class Syncer {
         }
 
         for (File file : dir.listFiles()) {
-            if (file.isFile()) {
+            if (file.isFile() && (!file.getName().equals(".sync"))) {
+                updateDotSync(dotSync, file);
             }
         }
     }
 
     public static void updateDotSync(File dotSync, File file) {
         String fileName = file.getName();
-        // String fileContent
+        String fileContent = Utility.readFile(file);
         Type collectionType = new TypeToken<Map<String, List<List<String>>>>(){}.getType();
         Gson gson = new Gson();
+        List<String> pair = new ArrayList<>();
+        String date, encoded;
 
 
         try {
             JsonReader jsonReader = new JsonReader(new FileReader(dotSync));
             Map<String, List<List<String>>> fileStatus = gson.fromJson(jsonReader, collectionType); 
+            List<List<String>> pairs = new ArrayList<>();
 
-            if (!fileStatus.containsKey(fileName)) {
-                List pair = new ArrayList<>();
-                String date = (new Date(file.lastModified())).toString();
-                
+            date = Utility.getFormattedTime(file);
+            encoded = Utility.getEncodedString(fileContent);
+            pair.add(date);
+            pair.add(encoded);
+
+            if (fileStatus == null) {
+                Map<String, List<List<String>>> temp = new HashMap<String, List<List<String>>>();
+
+                pairs.add(pair);
+                temp.put(fileName, pairs);
+                fileStatus = temp;
+
+                Utility.writeToDotSync(dotSync, fileStatus);
+            } else if (!fileStatus.containsKey(fileName)) {
+                pairs.add(pair);
+                fileStatus.put(fileName, pairs);
+
+                Utility.writeToDotSync(dotSync, fileStatus);
+            }else {
+                if (!encoded.equals(fileStatus.get(fileName).get(0).get(1))) {
+                    pairs = fileStatus.get(fileName);
+                    pairs.add(0, pair);
+                    fileStatus.put(fileName, pairs);
+
+                    Utility.writeToDotSync(dotSync, fileStatus);
+                } else if(date != fileStatus.get(fileName).get(0).get(0)) {
+                    file.setLastModified(Utility.getTimeForMod(fileStatus.get(fileName).get(0).get(0)));
+                }
             }
         } catch (Exception e) {
         }
--- src/Utility.java
+++ src/Utility.java
@@ -2,23 +2,85 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Utility {
     
     public static String getEncodedString(String preEncoded) {
-        String encoded = "";
+        StringBuilder builder = new StringBuilder();
 
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(preEncoded.getBytes(StandardCharsets.UTF_8));
-            encoded = Base64.getEncoder().encodeToString(hash);
+        
+            for (byte b : hash) {
+                builder.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+            }
 
-        } catch (NoSuchAlgorithmException exception) {
-            exception.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
 
-        return encoded;
+        return builder.toString();
     }
 
+    public static String readFile(File file) {
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                builder.append(line);   // Might need to add append("\n")
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return builder.toString();
+    }
+
+    public static void writeToDotSync(File dotSync, Map<String, List<List<String>>> fileStatus) {
+        try {
+            FileWriter writer = new FileWriter(dotSync, false);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String json = gson.toJson(fileStatus);
+
+            writer.write(json);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getFormattedTime(File file) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss Z");
+
+        return dateFormat.format(new Date(file.lastModified()));
+    }
+
+    public static long getTimeForMod(String time) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss Z");
+
+        try {
+            Date timeOnSync = dateFormat.parse(time);
+
+            System.out.println(time);
+            System.out.println(timeOnSync.toString());
+
+            return timeOnSync.getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return 0L;
+    }
 }
